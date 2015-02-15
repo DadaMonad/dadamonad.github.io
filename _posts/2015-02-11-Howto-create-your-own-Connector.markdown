6028c961e8624d242a4d1dfe04920e440f31f047
DadaMonad <kevin.jahns@rwth-aachen.de>
2015-02-15 16:29:52 +0000

    adding dependencies

---
+++ _posts/2015-02-11-Howto-create-your-own-Connector.markdown
@@ -0,0 +1,142 @@
+---
+layout: post
+title:  "Howto create your own Connector"
+date:   2015-02-11 00:56:45
+description: Howto create your own Connector
+categories:
+  - Yjs
+  - Connector
+---
+
+If you want to integrate Yjs in you existing project, you most likely already set up some infrastructure to communicate with your clients. Maybe you have a paid plan at [pusher](https://pusher.com/) or you are really into [Socket.io](http://socket.io) or you would just rather use [Matrix](http://matrix.org/) as an underlaying communication protocol. There is no need to be satisfied with the awesome [XMPP connector](https://github.com/rwth-acis/y-xmpp) to propagate document updates. Build your own connector! It's easy!
+
+In this article I'll explain how you can create your own connector. You can use _any_ communication protocol - whether it is a P2P based, federated, or client-server.
+
+### Choose a Sync Method
+
+There are two ways how your clients can synchronize with each other. The XMPP connector actually supports both sync methods. But it is totally fine if you only choose one of them:
+
+SyncAll Sync Method
+: This sync method originates from the original WebRTC connector. Everyone synchronizes with each other: Right after the connection is created, the peers exchange a state vector, and then send all the remaining Operations to the other users. After that, the connector only has to make sure that all sent operations reach all the other peers. I got feedback that this is the prefferred method to use, because there is no set-up involved. This method is totally fine and efficient for small networks, but is not well suited for a lot of users.
+
+Master-Slave Sync Method
+: Here, we have one or several *master* clients online that serve as the endpoint for the syncing process of the *slaves*. Therefore, you only have to sync once, even if there are thousands of users online. Still, operations can be published p2p (they don't have to go through the master-client). But you have to make sure that the master client(s) have a high uptime. They should not leave the session while a user syncs with it.
+
+You could create a master client with nodejs, that runs on our server/cloud.
+If you do not have (or do not want to have) a nodejs server instance, that can preserve the state of the shared document, you can choose the SyncAll sync method. Otherwise, you should go with the Master-Slave sync method.
+
+### Initializing the Connector
+Like you probably know, you initialize an instance of Y like this:
+{% highlight javascript %}
+  var y = new Y(connector)
+{% endhighlight %}
+
+We say, that the connector is now bound to Y. This process involves copying all the properties of the [ConnectorClass](https://github.com/rwth-acis/yjs/blob/master/lib/ConnectorClass.coffee) to the `connector`. At some point you have to *initialize* the connector with the chosen sync method, the *role*, and a unique *user_id*. You can do this at any moment, even after the user created properties on the shared document.
+
+{% highlight javascript %}
+// the communication protocol is completely set up
+// and is ready to propagate messages
+var when_bound_to_y = function(){
+  // when the connector is bound to Y,
+  // e.g. by creating a new instance of Y,
+  // initialize the connector with the required parameters.
+  // You always must specify `role`, `syncMethod`, and `user_id`
+  connector.init({
+    role : "slave", // or "master"
+    syncMethod : "syncAll", // or "master-slave"
+    user_id : user_id
+  });
+};
+
+if(connector.is_bound_to_y){
+  // The connector is already bound to Y, so we can execute
+  // `when_bound_to_y` immediately
+  when_bound_to_y();
+} else {
+  // The connector has not yet been bound to Y
+  // on_bound_to_y is called when the connector is bound to Y
+  connector.on_bound_to_y = when_bound_to_y;
+}
+{% endhighlight %}
+
+
+### Collaborator joins / leaves a session
+Whenever a user joins or leaves a session, you have to notify the connector class that it happened.
+
+Let the `protocol` variable be some communication protocol that supports events.
+
+{% highlight javascript %}
+protocol.on("userJoinedSession", function(user){
+  // a new user joined the session.
+  // Notify the connector class, if the connector
+  // is already initialized
+  if(connector.is_initialized){
+    // you must specify the id and
+    // the role ("master" or "slave") of the user
+    connector.userJoined(user.id, user.role);
+  }
+});
+{% endhighlight %}
+
+{% highlight javascript %}
+protocol.on("userLeftSession", function(user){
+  // a user left the session.
+  // Notify the connector class, if the connector
+  // is already initialized
+  if(connector.is_initialized){
+    // you must specify the id
+    connector.userLeft(user.id);
+  }
+});
+{% endhighlight %}
+
+### Specify how to send and receive messages
+You must specify how to *send*, *broadcast*, and how to *receive* messages. A message is a json object that may contain numbers, and strings. It is your responsibility that they arrive. In order to prevent encoding issues you may want to transform the object to a string with `JSON.stringify(message)` and parse it back with `JSON.parse(string_message)`.
+
+##### 1. How to send a message to a specific user
+
+{% highlight javascript %}
+connector.send = function(user_id, message){
+  protocol.send(user_id, JSON.stringify(message));
+}
+{% endhighlight %}
+
+##### 2. How to broadcast a message to all users
+
+Maybe your protocol supports broadcasting. Otherwise you could send the message to all users directly too.
+
+{% highlight javascript %}
+connector.broadcast = function(message){
+  protocol.broadcast(JSON.stringify(message));
+}
+{% endhighlight %}
+
+##### 3. How to receive a message
+
+You have to check if the connector is already initialized, otherwise the `receiveMessage` method may not exist yet!
+
+{% highlight javascript %}
+protocol.on("message", function(message){
+  // The client received a message
+  // Check if the connector is already initialized,
+  // only then forward the message to the connector class
+  if(connector.is_initialized){
+    connector.receiveMessage(user_id, JSON.parse(message));
+  }
+})
+{% endhighlight %}
+
+# Conclusion
+Creating a connector isn't that hard. It is something that distinguishes Yjs from similar frameworks. You can choose a communication protocol that fits your requirements, and integrates well into your existing infrastructure.
+
+The [webrtc connector](https://github.com/rwth-acis/y-webrtc) is a great starting point for your connector project, because it is pretty well documented. You can use it even if you choose the master-slave sync method.
+
+Contact me if you need any help creating your own connector.
+
+
+
+
+
+
+
+
---
+++ bower_components/yjs/build/node/ConnectorClass.js
@@ -0,0 +1,355 @@
+module.exports = {
+  init: function(options) {
+    var req;
+    req = (function(_this) {
+      return function(name, choices) {
+        if (options[name] != null) {
+          if ((choices == null) || choices.some(function(c) {
+            return c === options[name];
+          })) {
+            return _this[name] = options[name];
+          } else {
+            throw new Error("You can set the '" + name + "' option to one of the following choices: " + JSON.encode(choices));
+          }
+        } else {
+          throw new Error("You must specify " + name + ", when initializing the Connector!");
+        }
+      };
+    })(this);
+    req("syncMethod", ["syncAll", "master-slave"]);
+    req("role", ["master", "slave"]);
+    req("user_id");
+    if (typeof this.on_user_id_set === "function") {
+      this.on_user_id_set(this.user_id);
+    }
+    if (options.perform_send_again != null) {
+      this.perform_send_again = options.perform_send_again;
+    } else {
+      this.perform_send_again = true;
+    }
+    if (this.role === "master") {
+      this.syncMethod = "syncAll";
+    }
+    this.is_synced = false;
+    this.connections = {};
+    if (this.receive_handlers == null) {
+      this.receive_handlers = [];
+    }
+    this.connections = {};
+    this.current_sync_target = null;
+    this.sent_hb_to_all_users = false;
+    return this.is_initialized = true;
+  },
+  isRoleMaster: function() {
+    return this.role === "master";
+  },
+  isRoleSlave: function() {
+    return this.role === "slave";
+  },
+  findNewSyncTarget: function() {
+    var c, user, _ref;
+    this.current_sync_target = null;
+    if (this.syncMethod === "syncAll") {
+      _ref = this.connections;
+      for (user in _ref) {
+        c = _ref[user];
+        if (!c.is_synced) {
+          this.performSync(user);
+          break;
+        }
+      }
+    }
+    if (this.current_sync_target == null) {
+      this.setStateSynced();
+    }
+    return null;
+  },
+  userLeft: function(user) {
+    delete this.connections[user];
+    return this.findNewSyncTarget();
+  },
+  userJoined: function(user, role) {
+    var _base;
+    if (role == null) {
+      throw new Error("Internal: You must specify the role of the joined user! E.g. userJoined('uid:3939','slave')");
+    }
+    if ((_base = this.connections)[user] == null) {
+      _base[user] = {};
+    }
+    this.connections[user].is_synced = false;
+    if ((!this.is_synced) || this.syncMethod === "syncAll") {
+      if (this.syncMethod === "syncAll") {
+        return this.performSync(user);
+      } else if (role === "master") {
+        return this.performSyncWithMaster(user);
+      }
+    }
+  },
+  whenSynced: function(args) {
+    if (args.constructore === Function) {
+      args = [args];
+    }
+    if (this.is_synced) {
+      return args[0].apply(this, args.slice(1));
+    } else {
+      if (this.compute_when_synced == null) {
+        this.compute_when_synced = [];
+      }
+      return this.compute_when_synced.push(args);
+    }
+  },
+  onReceive: function(f) {
+    return this.receive_handlers.push(f);
+  },
+
+  /*
+   * Broadcast a message to all connected peers.
+   * @param message {Object} The message to broadcast.
+   *
+  broadcast: (message)->
+    throw new Error "You must implement broadcast!"
+  
+   *
+   * Send a message to a peer, or set of peers
+   *
+  send: (peer_s, message)->
+    throw new Error "You must implement send!"
+   */
+  performSync: function(user) {
+    var hb, o, _hb, _i, _len;
+    if (this.current_sync_target == null) {
+      this.current_sync_target = user;
+      this.send(user, {
+        sync_step: "getHB",
+        send_again: "true",
+        data: []
+      });
+      if (!this.sent_hb_to_all_users) {
+        this.sent_hb_to_all_users = true;
+        hb = this.getHB([]).hb;
+        _hb = [];
+        for (_i = 0, _len = hb.length; _i < _len; _i++) {
+          o = hb[_i];
+          _hb.push(o);
+          if (_hb.length > 30) {
+            this.broadcast({
+              sync_step: "applyHB_",
+              data: _hb
+            });
+            _hb = [];
+          }
+        }
+        return this.broadcast({
+          sync_step: "applyHB",
+          data: _hb
+        });
+      }
+    }
+  },
+  performSyncWithMaster: function(user) {
+    var hb, o, _hb, _i, _len;
+    this.current_sync_target = user;
+    this.send(user, {
+      sync_step: "getHB",
+      send_again: "true",
+      data: []
+    });
+    hb = this.getHB([]).hb;
+    _hb = [];
+    for (_i = 0, _len = hb.length; _i < _len; _i++) {
+      o = hb[_i];
+      _hb.push(o);
+      if (_hb.length > 30) {
+        this.broadcast({
+          sync_step: "applyHB_",
+          data: _hb
+        });
+        _hb = [];
+      }
+    }
+    return this.broadcast({
+      sync_step: "applyHB",
+      data: _hb
+    });
+  },
+  setStateSynced: function() {
+    var f, _i, _len, _ref;
+    if (!this.is_synced) {
+      this.is_synced = true;
+      if (this.compute_when_synced != null) {
+        _ref = this.compute_when_synced;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          f = _ref[_i];
+          f();
+        }
+        delete this.compute_when_synced;
+      }
+      return null;
+    }
+  },
+  receiveMessage: function(sender, res) {
+    var data, f, hb, o, sendApplyHB, send_again, _hb, _i, _j, _len, _len1, _ref, _results;
+    if (res.sync_step == null) {
+      _ref = this.receive_handlers;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        f = _ref[_i];
+        _results.push(f(sender, res));
+      }
+      return _results;
+    } else {
+      if (sender === this.user_id) {
+        return;
+      }
+      if (res.sync_step === "getHB") {
+        data = this.getHB(res.data);
+        hb = data.hb;
+        _hb = [];
+        if (this.is_synced) {
+          sendApplyHB = (function(_this) {
+            return function(m) {
+              return _this.send(sender, m);
+            };
+          })(this);
+        } else {
+          sendApplyHB = (function(_this) {
+            return function(m) {
+              return _this.broadcast(m);
+            };
+          })(this);
+        }
+        for (_j = 0, _len1 = hb.length; _j < _len1; _j++) {
+          o = hb[_j];
+          _hb.push(o);
+          if (_hb.length > 30) {
+            sendApplyHB({
+              sync_step: "applyHB_",
+              data: _hb
+            });
+            _hb = [];
+          }
+        }
+        sendApplyHB({
+          sync_step: "applyHB",
+          data: _hb
+        });
+        if ((res.send_again != null) && this.perform_send_again) {
+          send_again = (function(_this) {
+            return function(sv) {
+              return function() {
+                hb = _this.getHB(sv).hb;
+                return _this.send(sender, {
+                  sync_step: "applyHB",
+                  data: hb,
+                  sent_again: "true"
+                });
+              };
+            };
+          })(this)(data.state_vector);
+          return setTimeout(send_again, 3000);
+        }
+      } else if (res.sync_step === "applyHB") {
+        this.applyHB(res.data, sender === this.current_sync_target);
+        if ((this.syncMethod === "syncAll" || (res.sent_again != null)) && (!this.is_synced) && ((this.current_sync_target === sender) || (this.current_sync_target == null))) {
+          this.connections[sender].is_synced = true;
+          return this.findNewSyncTarget();
+        }
+      } else if (res.sync_step === "applyHB_") {
+        return this.applyHB(res.data, sender === this.current_sync_target);
+      }
+    }
+  },
+  parseMessageFromXml: function(m) {
+    var parse_array, parse_object;
+    parse_array = function(node) {
+      var n, _i, _len, _ref, _results;
+      _ref = node.children;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        n = _ref[_i];
+        if (n.getAttribute("isArray") === "true") {
+          _results.push(parse_array(n));
+        } else {
+          _results.push(parse_object(n));
+        }
+      }
+      return _results;
+    };
+    parse_object = function(node) {
+      var int, json, n, name, value, _i, _len, _ref, _ref1;
+      json = {};
+      _ref = node.attrs;
+      for (name in _ref) {
+        value = _ref[name];
+        int = parseInt(value);
+        if (isNaN(int) || ("" + int) !== value) {
+          json[name] = value;
+        } else {
+          json[name] = int;
+        }
+      }
+      _ref1 = node.children;
+      for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+        n = _ref1[_i];
+        name = n.name;
+        if (n.getAttribute("isArray") === "true") {
+          json[name] = parse_array(n);
+        } else {
+          json[name] = parse_object(n);
+        }
+      }
+      return json;
+    };
+    return parse_object(m);
+  },
+  encodeMessageToXml: function(m, json) {
+    var encode_array, encode_object;
+    encode_object = function(m, json) {
+      var name, value;
+      for (name in json) {
+        value = json[name];
+        if (value == null) {
+
+        } else if (value.constructor === Object) {
+          encode_object(m.c(name), value);
+        } else if (value.constructor === Array) {
+          encode_array(m.c(name), value);
+        } else {
+          m.setAttribute(name, value);
+        }
+      }
+      return m;
+    };
+    encode_array = function(m, array) {
+      var e, _i, _len;
+      m.setAttribute("isArray", "true");
+      for (_i = 0, _len = array.length; _i < _len; _i++) {
+        e = array[_i];
+        if (e.constructor === Object) {
+          encode_object(m.c("array-element"), e);
+        } else {
+          encode_array(m.c("array-element"), e);
+        }
+      }
+      return m;
+    };
+    if (json.constructor === Object) {
+      return encode_object(m.c("y", {
+        xmlns: "http://y.ninja/connector-stanza"
+      }), json);
+    } else if (json.constructor === Array) {
+      return encode_array(m.c("y", {
+        xmlns: "http://y.ninja/connector-stanza"
+      }), json);
+    } else {
+      throw new Error("I can't encode this json!");
+    }
+  },
+  setIsBoundToY: function() {
+    if (typeof this.on_bound_to_y === "function") {
+      this.on_bound_to_y();
+    }
+    delete this.when_bound_to_y;
+    return this.is_bound_to_y = true;
+  }
+};
